f33699a95d60097183c09d99463b1630b4fe70ae
andrejmalykhin <Andrej.Malykhin.13@gmail.com>
2019-02-02 17:22:29 +0200

    60. Spring REST - Using @PathVariable for REST Endpoints

--- src/main/java/com/giggs13/rest/controller/StudentApiController.java
+++ src/main/java/com/giggs13/rest/controller/StudentApiController.java
@@ -3,9 +3,11 @@ package com.giggs13.rest.controller;
 import com.giggs13.rest.entity.Student;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,13 +15,27 @@ import java.util.List;
 @RequestMapping("/api/students")
 public class StudentApiController {
 
-    @GetMapping(produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public List<Student> getStudents() {
-        List<Student> students = new ArrayList<>();
-        students.add(new Student("Poornima", "Patel"));
-        students.add(new Student("Mario", "Rossi"));
-        students.add(new Student("Mary", "Smith"));
+    private List<Student> students;
 
+    @PostConstruct
+    private void init() {
+        initStudents();
+    }
+
+    private void initStudents() {
+        students = new ArrayList<>();
+        students.add(new Student(1, "Poornima", "Patel"));
+        students.add(new Student(2, "Mario", "Rossi"));
+        students.add(new Student(3, "Mary", "Smith"));
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public List<Student> getAll() {
         return students;
+    }
+
+    @GetMapping(path = "/{id}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public Student getById(@PathVariable int id) {
+        return students.get(id);
     }
 }
--- src/main/java/com/giggs13/rest/entity/Student.java
+++ src/main/java/com/giggs13/rest/entity/Student.java
@@ -9,6 +9,7 @@ import lombok.*;
 @ToString
 public class Student {
 
+    private int id;
     private String firstName;
     private String lastName;
 }
